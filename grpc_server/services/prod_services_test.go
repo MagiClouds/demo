1c681c34e7b946b204a4f31694f64f042f48df0f
magiclouds <915959688@qq.com>
2020-06-06 13:51:56 +0800

    .

--- grpc_server/services/prod_services_test.go
+++ grpc_server/services/prod_services_test.go
@@ -8,3 +8,13 @@ import (
 func TestInt32(t *testing.T)  {
 	fmt.Println(int32(0))
 }
+
+func TestMap(t *testing.T)  {
+	m := map[string]interface{}{"a":1,
+		"b":"haha"}
+
+	for k := range m {
+		fmt.Println(k)
+	}
+
+}
